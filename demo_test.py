a2bb70ce262cffafabd73e94b3b4cf151b5810a8
taidora1234 <s.tanechka98@mail.ru>
2023-05-05 15:12:39 +0300

    new

---
+++ demo_test.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+import numpy as np
+
+df = pd.read_csv('housing.csv')
+
+if st.button('Отобразить первые пять строк'):
+    st.write(df.head())
+
+if st.button('Обучить модель'):
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('MEDV', axis=1),
+                                                        df['MEDV'],
+                                                        test_size=0.2,
+                                                        random_state=2100)
+    st.write('Разделили данные и передали в обучение')
+    regr_model = XGBRegressor()
+    regr_model.fit(X_train, y_train)
+    pred = regr_model.predict(X_test)
+    st.write('Обучили модель, MAE = ' + str(mean_absolute_error(y_test, pred)))
+
+
+
+
+if st.button('Обучить модель'):
+    train_size = st.slider('Выберите размер обучающей выборки:', min_value=0.1, max_value=0.9, value=0.8, step=0.1)
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('MEDV', axis=1),
+                                                        df['MEDV'],
+                                                        test_size=1-train_size,
+                                                        random_state=2100)
+    st.write('Разделили данные и передали в обучение')
+    regr_model = XGBRegressor()
+    regr_model.fit(X_train, y_train)
+    pred = regr_model.predict(X_test)
+    st.write('Обучили модель, MAE = ' + str(mean_absolute_error(y_test, pred)))
+    
